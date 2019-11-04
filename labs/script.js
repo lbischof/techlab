cf4b958f6c715c957006a426889bbb1def04959b
Lorenz Bischof <lorenzbischof@gmail.com>
2019-11-04 17:00:41 +0100

    Add initial modal prompt

--- labs/script.js
+++ labs/script.js
@@ -1,4 +1,33 @@
+function createUsernamePrompt() {
+    var modal = document.createElement('div')
+    modal.setAttribute('id', 'usernamePrompt')
+
+    // TODO: add button to skip prompt
+    modal.innerHTML = [
+        '<p>Please enter your Techlab Username:</p>',
+        '<input id="usernamePromptInput" type="text"></input>',
+        '<button id="usernamePromptButton">Set Username</button>'
+    ].join('\n');
+    document.body.appendChild(modal)
+
+    let btn = document.getElementById('usernamePromptButton')
+    let input = document.getElementById('usernamePromptInput')
+    btn.addEventListener('click', function(e) {
+        console.log('clicked')
+        localStorage.setItem("username", input.value)
+        // TODO: close modal
+    })
+}
+
 (function() {
+    console.log(localStorage.getItem("username"))
+
+    let username = localStorage.getItem("username")
+    if (!username) {
+        console.log("Please set a username")
+        createUsernamePrompt()
+    }
+
     // md-content__inner
     const replaceOnDocument = (pattern, string, {target = document.body} = {}) => {
         // Handle `string` — see the last section
@@ -9,5 +38,5 @@
             .filter(({nodeType}) => nodeType === document.TEXT_NODE)
             .forEach((textNode) => textNode.textContent = textNode.textContent.replace(pattern, string)));
     };
-    replaceOnDocument(/\[USER\]/g, "lbischof", document.getElementsByClassName("md-content__inner")[0]);
+    replaceOnDocument(/\[USER\]/g, username, document.getElementsByClassName("md-content__inner")[0]);
 })();
